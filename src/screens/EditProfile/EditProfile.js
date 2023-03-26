01b3df108c64efa68661e69ef0f059a42b3f7601
Malki-dev848 <IT19187938@gmail.com>
2023-03-26 14:31:58 +0530

    edit editprofile frontend

--- src/screens/EditProfile/EditProfile.js
+++ src/screens/EditProfile/EditProfile.js
@@ -1,7 +1,6 @@
 import React from "react";
 import{View,Text,Image,StyleSheet,TextInput, useWindowDimensions,Pressable, TouchableOpacity, ImageBackground, ScrollView} from "react-native";
 import CustomHeader from "../../components/CustomHeader";
-import { FlatList } from "react-native-gesture-handler";
 
 
 
